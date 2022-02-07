1f1380da5286ff68cc8f3e69c67204d94f650b2e
MilenPlamenov <milenkrasy@abv.bg>
2022-02-07 21:22:42 +0200

    refactored the urls in the template

--- notesApp/notes_management/urls.py
+++ notesApp/notes_management/urls.py
@@ -5,7 +5,7 @@ from notesApp.notes_management.views import NotesListView, NoteCreate, NotesDeta
 urlpatterns = [
     path('', NotesListView.as_view(), name="notes list"),
     path('create/', NoteCreate.as_view(), name="note create"),
-    path('<pk>/', NotesDetailView.as_view(), name="note detail"),
+    path('<int:pk>/', NotesDetailView.as_view(), name="note detail"),
     path('update/<int:pk>/', NotesUpdateView.as_view(), name="note update"),
     path('delete/<int:pk>/', NotesDeleteView.as_view(), name="note delete"),
 ]
