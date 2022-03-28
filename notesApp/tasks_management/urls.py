c97771aedce28dbc67f0f3fed0458cb334fb8cd2
MilenPlamenov <milenkrasy@abv.bg>
2022-03-28 21:30:08 +0300

    Added option to create tasks and modified the urls

--- notesApp/tasks_management/urls.py
+++ notesApp/tasks_management/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
-urlpatterns = [
+from notesApp.tasks_management.views import TaskCreate
 
+urlpatterns = [
+    path('create/', TaskCreate.as_view(), name="task create"),
 ]
--- notesApp/tasks_management/views.py
+++ notesApp/tasks_management/views.py
@@ -1,2 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
 
+from notesApp.tasks_management.models import Task
+
+
+class TaskCreate(CreateView):
+    model = Task
+    template_name = "tasks/create_task.html"
+    success_url = '/'
+    fields = ["name", "text", "is_done"]
